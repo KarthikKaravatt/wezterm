8dbbc6e3f5a557b73a7b99ad1f7e07d19cc40ffd
KK <82811712+KarthikKaravatt@users.noreply.github.com>
2024-11-05 11:25:31 +0800

    Set cwd as home

--- shell.lua
+++ shell.lua
@@ -3,6 +3,7 @@ local wezterm = require 'wezterm'
 local M = {}
 
 function M.apply_to_config(config)
+  config.default_cwd = wezterm.home_dir
   if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     -- For window also get rid of the startup message
     config.default_prog = { 'pwsh.exe', '-NoLogo' }
